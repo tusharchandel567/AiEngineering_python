c56b19f6a57da6d096bdf4da26f303fe5ef7c4f1
tushar chandel <sc970557@gmail.com>
2026-09-22 10:06:02 +0530

    today 22 september learning ai engineering python - learn(bit wise operator total 6 )

---
+++ day30.py
@@ -0,0 +1,40 @@
+#Bit wise operators
+# 1. << 
+a = 10
+b = 12
+c=a<<3
+d=b>>2
+print(c)
+print(d)
+
+#2. >>
+a=20
+b=30
+c=a>>2 & b>>2
+print(c)
+
+#3 ! not Equal to 
+a=10
+b=20
+c=a!=b
+print(c)
+
+#4. & and
+a=10
+b=20
+c=a&b
+print(c)
+
+#5. ~ complement
+a=10
+b=20
+c=~a
+d=~b
+print(c)
+print(d)
+
+#6. ^ Xor
+a=10
+b=20
+c=a^b
+print(c)
